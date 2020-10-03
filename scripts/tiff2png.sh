03d5c3bccf87e65101af8fe4f7d714bf4f936a54
Rodolfo <rodolfo.lotte@gmail.com>
2020-10-03 18:02:19 -0300

    including tiff2png.sh script

---
+++ scripts/tiff2png.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Usage: ./tiff2png PATH_TO_TIFF_FOLDER
+# Source: https://stackoverflow.com/questions/48944819/image-open-gives-error-cannot-identify-image-file
+#         https://gis.stackexchange.com/questions/255336/convert-tif-to-png-without-normalizing-alpha
+#         https://gis.stackexchange.com/questions/246934/translating-geotiff-to-16-bit-png-with-gdal
+for entry in $1*
+do
+  if [ -f "$entry" ];then
+    dir=$(dirname "$entry")"/"
+    filename=$(basename -- "$entry")
+    extension="${filename##*.}"
+    name="${filename%.*}"
+    gdal_translate -of PNG -ot UInt16 $dir$filename $dir$name".PNG"
+    rm $entry
+    rm $dir$name".PNG.aux.xml"
+  fi
+done
